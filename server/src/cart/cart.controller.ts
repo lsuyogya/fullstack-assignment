4e6cb8dac15986647d5a1a9f9b0b674930947481
Suyogya Luitel <lsuyogya@gmail.com>
2023-07-07 07:41:50 +0545

    feat: cart logic

--- server/src/cart/cart.controller.ts
+++ server/src/cart/cart.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { CartService } from './cart.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private cartService: CartService) {}
+  @UseGuards(AuthGuard)
+  @Get()
+  viewCart(@Request() req) {
+    this.cartService.viewCart({ userId: req.user.userId });
+  }
+}
--- server/src/cart/cart.service.ts
+++ server/src/cart/cart.service.ts
@@ -1,4 +1,40 @@
+import { PrismaService } from 'src/database/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(private prisma: PrismaService) {}
+
+  async viewCart({ userId }) {
+    return await this.prisma.cart.findUnique({
+      where: { userId: userId },
+      include: { books: true },
+    });
+  }
+
+  async addToCart({ userId, bookId }) {
+    return await this.prisma.cart.update({
+      where: { userId: userId },
+      data: {
+        books: {
+          connect: {
+            bookId: bookId,
+          },
+        },
+      },
+    });
+  }
+
+  async delFromCart({ userId, bookId }) {
+    return await this.prisma.cart.update({
+      where: { userId: userId },
+      data: {
+        books: {
+          disconnect: {
+            bookId: bookId,
+          },
+        },
+      },
+    });
+  }
+}
